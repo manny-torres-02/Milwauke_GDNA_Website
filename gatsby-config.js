1000b520f9bbeb8e330d83842c7866150f6fbd10
Manuel Torres <manueljosetorres02@gmail.com>
2023-05-01 21:55:01 -0500

    updates.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
+    title: "Milwaukee Garden District Neighborhood Association",
     description: "Official Contentful Gatsby Starter",
   },
   plugins: [
