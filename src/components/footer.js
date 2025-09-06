98fa263b02bee07a54c261883fd66ac8a6549c1a
Manuel Torres <manueljosetorres02@gmail.com>
2025-09-06 14:59:01 -0500

    footer fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,32 +12,45 @@ const Footer = () => (
       <section className={styles.socialSection}>
         <h3 className={styles.sectionTitle}>Follow Us</h3>
         <div className={styles.socialIcons}>
-          <a 
-            href="https://www.facebook.com/MilwaukeeGardenDistrict/" 
+          <a
+            href="https://www.facebook.com/MilwaukeeGardenDistrict/"
             aria-label="Visit our Facebook page"
             className={styles.socialLink}
           >
-            <img className={styles.socialIcon} src={facebookIcon} alt="Facebook" />
+            <img
+              className={styles.socialIcon}
+              src={facebookIcon}
+              alt="Facebook"
+            />
           </a>
-          <a 
+          <a
             href="https://www.instagram.com/mkegdna/"
             aria-label="Visit our Instagram page"
             className={styles.socialLink}
           >
-            <img className={styles.socialIcon} src={instagramIcon} alt="Instagram" />
+            <img
+              className={styles.socialIcon}
+              src={instagramIcon}
+              alt="Instagram"
+            />
           </a>
         </div>
       </section>
-      
+
       <section className={styles.donateSection}>
         <h3 className={styles.sectionTitle}>Donate to MGDNA</h3>
-        <img className={styles.qrCode} src={paypalQRCode} alt="PayPal QR code for donations" />
+        <img
+          className={styles.qrCode}
+          src={paypalQRCode}
+          alt="PayPal QR code for donations"
+        />
       </section>
-      
+
       <section className={styles.contactSection}>
         <h3 className={styles.sectionTitle}>Contact</h3>
         <address className={styles.address}>
-          P.O. Box 70876<br />
+          P.O. Box 70876
+          <br />
           Milwaukee, WI 53207
         </address>
       </section>
