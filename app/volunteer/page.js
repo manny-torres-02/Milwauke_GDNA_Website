12884964585ceb787893e72638e2a5c0c2a76402
Manuel Torres <manueljosetorres02@gmail.com>
2025-11-03 23:30:19 -0500

    merge nextjs, ui/ux fixes

--- app/volunteer/page.js
+++ app/volunteer/page.js
@@ -5,8 +5,10 @@ import paypalQRCode from '../../src/assets/mgdna_paypal_QR_Code.png'
 import '../../src/components/volunteer.css'
 
 export const metadata = {
-  title: 'Donate and Volunteer | Milwaukee Garden District Neighborhood Association',
-  description: 'Support the Milwaukee Garden District Neighborhood Association through donations and volunteering.',
+  title:
+    'Donate and Volunteer | Milwaukee Garden District Neighborhood Association',
+  description:
+    'Support the Milwaukee Garden District Neighborhood Association through donations and volunteering.',
 }
 
 export default function VolunteerPage() {
@@ -30,6 +32,28 @@ export default function VolunteerPage() {
               P.O. Box 70876, Milwaukee WI 53207
             </h1>
           </div>
+        </div>
+        <div style={{ marginTop: '24px' }}>
+          <h1> Volunteer to set up Winter Wonderland</h1>
+          <p>
+            Our Winter Wonderland event is right around the corner, and we need
+            your help to make it shine! ❄️✨
+          </p>
+
+          <p>
+            Join our volunteer team to help with setup, decorations, and
+            creating a cozy, festive atmosphere that everyone can enjoy. Whether
+            you have an hour or a few, your support makes a huge difference!
+          </p>
+          <a
+            href="https://www.signupgenius.com/go/20F054DABAF2BAAF49-59680253-winter#/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ctaButton"
+            aria-label="Volunteer for Winter Wonderland set up"
+          >
+            Volunteer for Winter Wonderland set up
+          </a>
         </div>
         <Image
           alt="Hero Photo, Hands"
