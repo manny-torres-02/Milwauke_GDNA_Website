6e0429c970961f9671be55a115aece13e1abe388
Manuel Torres <manueljosetorres02@gmail.com>
2026-03-23 21:03:16 -0500

    migrated changes

---
+++ .next/server/chunks/952.js
@@ -0,0 +1,192 @@
+exports.id=952,exports.ids=[952],exports.modules={965:()=>{},12636:a=>{a.exports={container:"navigation_container__CZWBr",navigation:"navigation_navigation__CyL8D",navigationItem:"navigation_navigationItem__aSMII",logoLink:"navigation_logoLink__3vZ8n",logo:"navigation_logo__WMeUG",hamburger:"navigation_hamburger___0IRj",backdrop:"navigation_backdrop__9dj2M",navigationOpen:"navigation_navigationOpen__q2WlL"}},24932:(a,b,c)=>{Promise.resolve().then(c.t.bind(c,54160,23)),Promise.resolve().then(c.t.bind(c,31603,23)),Promise.resolve().then(c.t.bind(c,68495,23)),Promise.resolve().then(c.t.bind(c,75170,23)),Promise.resolve().then(c.t.bind(c,77526,23)),Promise.resolve().then(c.t.bind(c,78922,23)),Promise.resolve().then(c.t.bind(c,29234,23)),Promise.resolve().then(c.t.bind(c,12263,23)),Promise.resolve().then(c.bind(c,82146))},25958:(a,b,c)=>{"use strict";c.d(b,{A:()=>k});var d=c(75338);c(74515),c(71125),c(45161);var e=c(40671),f=c(59865),g=c(27864),h=c(40649),i=c(49545);let j=()=>(0,d.jsx)(g.A,{as:"footer",children:(0,d.jsxs)("div",{className:h.container,children:[(0,d.jsxs)("section",{className:h.socialSection,children:[(0,d.jsx)("h3",{className:h.sectionTitle,children:"Follow Us"}),(0,d.jsxs)("div",{className:h.socialIcons,children:[(0,d.jsx)("a",{href:"https://www.facebook.com/MilwaukeeGardenDistrict/","aria-label":"Visit our Facebook page",className:h.socialLink,children:(0,d.jsx)("svg",{className:h.socialIcon,viewBox:"0 0 24 24",width:"24",height:"24",children:(0,d.jsx)("path",{fill:"currentColor",d:"M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"})})}),(0,d.jsx)("a",{href:"https://www.instagram.com/mkegdna/","aria-label":"Visit our Instagram page",className:h.socialLink,children:(0,d.jsx)("svg",{className:h.socialIcon,viewBox:"0 0 24 24",width:"24",height:"24",children:(0,d.jsx)("path",{fill:"currentColor",d:"M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"})})})]})]}),(0,d.jsxs)("section",{className:h.donateSection,children:[(0,d.jsx)("h3",{className:h.sectionTitle,children:"Donate to MGDNA"}),(0,d.jsx)(f.default,{className:h.qrCode,src:i.default,alt:"PayPal QR code for donations",width:200,height:200})]}),(0,d.jsxs)("section",{className:h.contactSection,children:[(0,d.jsx)("h3",{className:h.sectionTitle,children:"Contact"}),(0,d.jsxs)("address",{className:h.address,children:["P.O. Box 70876",(0,d.jsx)("br",{}),"Milwaukee, WI 53207"]})]})]})}),k=({children:a,logoUrl:b})=>(0,d.jsxs)(d.Fragment,{children:[(0,d.jsx)(e.default,{logoUrl:b}),(0,d.jsx)("main",{children:a}),(0,d.jsx)(j,{})]})},27697:(a,b,c)=>{"use strict";c.r(b),c.d(b,{default:()=>f,metadata:()=>e});var d=c(75338);c(71125),c(45161);let e={title:"Milwaukee Garden District Neighborhood Association",description:"Official Contentful Gatsby Starter"};function f({children:a}){return(0,d.jsx)("html",{lang:"en",children:(0,d.jsx)("body",{children:a})})}},27864:(a,b,c)=>{"use strict";c.d(b,{A:()=>e});var d=c(75338);c(74515);let e=({children:a,as:b="div"})=>(0,d.jsx)(b,{style:{maxWidth:"var(--size-max-width)",margin:"0 auto",padding:"var(--space-2xl) var(--size-gutter)"},children:a})},37413:()=>{},39817:(a,b,c)=>{"use strict";c.r(b),c.d(b,{default:()=>d});let d={src:"/_next/static/media/mgdna_paypal_QR_Code.4f7ade32.png",height:499,width:499,blurDataURL:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAMAAADz0U65AAAAKlBMVEXt7vPIy9jo6u/S2eTQ0t3CxtS4vc7e3ub4+PqHpMCPpsF3jau+1eNWZYsXTxY1AAAACXBIWXMAABYlAAAWJQFJUiTwAAAAOElEQVR4nB3GyRHAMAwDsaUo+krSf7uZMV4AQ5XBOy0dg5LuLCr+Hg+o1n47N3NK4CMpAzBF1foBIFEA4t212skAAAAASUVORK5CYII=",blurWidth:8,blurHeight:8}},40649:a=>{a.exports={container:"footer_container__OJLlx",socialSection:"footer_socialSection__hsqGt",donateSection:"footer_donateSection__ss_Fw",contactSection:"footer_contactSection__do2qW",sectionTitle:"footer_sectionTitle__0Giwk",socialIcons:"footer_socialIcons__V8ulD",socialLink:"footer_socialLink__6nCJk",socialIcon:"footer_socialIcon__jtPeV",qrCode:"footer_qrCode__7B9Sm",address:"footer_address__eAcjJ",ctaButton:"footer_ctaButton__tpqZh",facebook_icon:"footer_facebook_icon__ADTky"}},40671:(a,b,c)=>{"use strict";c.d(b,{default:()=>d});let d=(0,c(97954).registerClientReference)(function(){throw Error("Attempted to call the default export of \"/Users/manueltorres/Documents/Work_Professional/Milwaukee_GDNA_Website/src/components/navigation-next.js\" from the server, but it's on the client. It's not possible to invoke a client function from the server, it can only be rendered as a Component or passed to props of a Client Component.")},"/Users/manueltorres/Documents/Work_Professional/Milwaukee_GDNA_Website/src/components/navigation-next.js","default")},45161:()=>{},49545:(a,b,c)=>{"use strict";c.r(b),c.d(b,{default:()=>d});let d={src:"/_next/static/media/mgdna_paypal_QR_Code.4f7ade32.png",height:499,width:499,blurDataURL:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAMAAADz0U65AAAAKlBMVEXt7vPIy9jo6u/S2eTQ0t3CxtS4vc7e3ub4+PqHpMCPpsF3jau+1eNWZYsXTxY1AAAACXBIWXMAABYlAAAWJQFJUiTwAAAAOElEQVR4nB3GyRHAMAwDsaUo+krSf7uZMV4AQ5XBOy0dg5LuLCr+Hg+o1n47N3NK4CMpAzBF1foBIFEA4t212skAAAAASUVORK5CYII=",blurWidth:8,blurHeight:8}},57253:(a,b,c)=>{"use strict";c.d(b,{default:()=>j});var d=c(21124),e=c(38301),f=c(3991),g=c.n(f),h=c(42378),i=c(12636);let j=({logoUrl:a})=>{let[b,c]=(0,e.useState)(!1),f=(0,h.usePathname)();return(0,d.jsxs)(d.Fragment,{children:[b&&(0,d.jsx)("div",{className:i.backdrop,onClick:()=>c(!1)}),(0,d.jsxs)("nav",{role:"navigation",className:i.container,"aria-label":"Main",children:[(0,d.jsx)(g(),{href:"/",className:i.logoLink,children:a&&(0,d.jsx)("img",{className:i.logo,src:a,alt:"Milwaukee GDNA Logo"})}),(0,d.jsxs)("button",{className:i.hamburger,onClick:()=>c(!b),"aria-label":"Toggle navigation","aria-expanded":b,children:[(0,d.jsx)("span",{}),(0,d.jsx)("span",{}),(0,d.jsx)("span",{})]}),(0,d.jsxs)("ul",{className:`${i.navigation} ${b?i.navigationOpen:""}`,children:[(0,d.jsx)("li",{className:i.navigationItem,children:(0,d.jsx)(g(),{href:"/",className:"/"===f?"active":"",onClick:()=>c(!1),children:"Home"})}),(0,d.jsx)("li",{className:i.navigationItem,children:(0,d.jsx)(g(),{href:"/blog",className:"/blog"===f?"active":"",onClick:()=>c(!1),children:"Blog"})}),(0,d.jsx)("li",{className:i.navigationItem,children:(0,d.jsx)(g(),{href:"/board",className:"/board"===f?"active":"",onClick:()=>c(!1),children:"The Board"})}),(0,d.jsx)("li",{className:i.navigationItem,children:(0,d.jsx)(g(),{href:"/calendar",onClick:()=>c(!1),children:"Calendar"})}),(0,d.jsx)("li",{className:i.navigationItem,children:(0,d.jsx)(g(),{href:"/volunteer",className:"/volunteer"===f?"active":"",onClick:()=>c(!1),children:"Donate and Volunteer"})}),(0,d.jsx)("li",{className:i.navigationItem,children:(0,d.jsx)(g(),{href:"/resources",className:"/resources"===f?"active":"",onClick:()=>c(!1),children:"Links"})})]})]})]})}},71125:()=>{},71780:(a,b,c)=>{Promise.resolve().then(c.t.bind(c,81170,23)),Promise.resolve().then(c.t.bind(c,23597,23)),Promise.resolve().then(c.t.bind(c,36893,23)),Promise.resolve().then(c.t.bind(c,89748,23)),Promise.resolve().then(c.t.bind(c,6060,23)),Promise.resolve().then(c.t.bind(c,7184,23)),Promise.resolve().then(c.t.bind(c,69576,23)),Promise.resolve().then(c.t.bind(c,73041,23)),Promise.resolve().then(c.t.bind(c,51384,23))},96103:(a,b,c)=>{"use strict";c.d(b,{$3:()=>o,B4:()=>v,WT:()=>q,fv:()=>p,iH:()=>r,j5:()=>u,p_:()=>t,t_:()=>s});let d="f0rnkukgphm9",e="AtGq2HvbjgSR2LOKy3s5mrJLMBgjZrSXYAV8gd8FvdM",f=process.env.CONTENTFUL_ENVIRONMENT||"master",g=`https://graphql.contentful.com/content/v1/spaces/${d}/environments/${f}`;async function h(a,b={}){if(!d||!e)return console.error("Missing Contentful env vars: CONTENTFUL_SPACE_ID or CONTENTFUL_ACCESS_TOKEN"),null;try{let c=await fetch(g,{method:"POST",headers:{"Content-Type":"application/json",Authorization:`Bearer ${e}`},body:JSON.stringify({query:a,variables:b})}),d=await c.json();if(!c.ok||d.errors)return console.error("Contentful GraphQL error:",d.errors||c.statusText),null;return d.data}catch(a){return console.error("Contentful GraphQL fetch failed:",a),null}}async function i({query:a,root:b,limit:c=20,maxPages:d=50,variables:e={}}){let f=[],g=0;for(let i=0;i<d;i++){let d=await h(a,{...e,limit:c,skip:g});if(!d)break;let i=d?.[b],j=i?.items||[];if(f.push(...j),j.length<c)break;g+=c}return f}function j(a){return a?{fields:{title:a.title||null,description:a.description||null,file:{url:a.url?a.url.replace(/^https:/,""):null}}}:null}function k(a){return a?{sys:{id:a.sys?.id},fields:{name:a.name,shortBio:a.shortBio?.json||null,image:j(a.image)}}:null}function l(a){return a?{sys:{id:a.sys?.id},fields:{title:a.title,slug:a.slug,publishDate:a.publishDate,tags:a.tags||[],description:a.description?.json||null,body:a.body?.json||null,bodyLinks:a.body?.links||null,heroImage:j(a.heroImage),author:a.author?k(a.author):null}}:null}function m(a){return a?{sys:{id:a.sys?.id},fields:{name:a.name,biography:a.biography||"",bioPhotos:j(a.bioPhotos)}}:null}function n(a){return a?{sys:{id:a.sys?.id},fields:{fileDescription:a.fileDescription||null,downloadableFile:j(a.downloadableFile)}}:null}async function o(){let a=`
+    query GetAllBlogPosts($limit: Int!, $skip: Int!) {
+      blogPostCollection(limit: $limit, skip: $skip, order: publishDate_DESC) {
+        items {
+          sys {
+            id
+          }
+          title
+          slug
+          publishDate
+          tags
+          description {
+            json
+          }
+          body {
+            json
+          }
+          heroImage {
+            url
+            title
+            description
+            width
+            height
+          }
+          author {
+            sys {
+              id
+            }
+            name
+            shortBio {
+              json
+            }
+            image {
+              url
+              title
+              description
+              width
+              height
+            }
+          }
+        }
+      }
+    }
+  `;return(await i({query:a,root:"blogPostCollection",limit:20})).map(l)}async function p(a){let b=`
+    query GetBlogPost($slug: String!, $limit: Int = 1) {
+      blogPostCollection(where: { slug: $slug }, limit: $limit) {
+        items {
+          sys {
+            id
+          }
+          title
+          slug
+          publishDate
+          tags
+          description {
+            json
+          }
+          body {
+            json
+            links {
+              assets {
+                block {
+                  sys {
+                    id
+                  }
+                  url
+                  title
+                  description
+                  width
+                  height
+                }
+              }
+            }
+          }
+          heroImage {
+            url
+            title
+            description
+            width
+            height
+          }
+          author {
+            sys {
+              id
+            }
+            name
+            shortBio {
+              json
+            }
+            image {
+              url
+              title
+              description
+              width
+              height
+            }
+          }
+        }
+      }
+    }
+  `,c=await h(b,{slug:a}),d=c?.blogPostCollection?.items?.[0];return d?l(d):null}async function q(){let a=`
+    query GetAllBlogSlugs($limit: Int!, $skip: Int!) {
+      blogPostCollection(limit: $limit, skip: $skip) {
+        items {
+          slug
+        }
+      }
+    }
+  `;return(await i({query:a,root:"blogPostCollection",limit:200})).map(a=>a.slug).filter(Boolean)}async function r(a){let b=`
+    query GetPerson($id: String!) {
+      person(id: $id) {
+        sys {
+          id
+        }
+        name
+        shortBio {
+          json
+        }
+        image {
+          url
+          title
+          description
+          width
+          height
+        }
+      }
+    }
+  `,c=await h(b,{id:a});return c?.person?k(c.person):null}async function s(){let a=`
+    query GetAllBoardMembers($limit: Int = 100) {
+      boardMember2Collection(limit: $limit) {
+        items {
+          sys {
+            id
+          }
+          name
+          biography
+          bioPhotos {
+            url
+            title
+            description
+            width
+            height
+          }
+        }
+      }
+    }
+  `,b=await h(a);return b?(b.boardMember2Collection?.items||[]).map(m):[]}async function t(a){let b=`
+    query GetAsset($id: String!) {
+      asset(id: $id) {
+        url
+        title
+        description
+        width
+        height
+      }
+    }
+  `,c=await h(b,{id:a});return c?.asset?j(c.asset):null}async function u(){let a=`
+    query GetCalendar($limit: Int = 1) {
+      calendarCollection(limit: $limit) {
+        items {
+          sys {
+            id
+          }
+          calendarPdf {
+            url
+            title
+            description
+            width
+            height
+          }
+        }
+      }
+    }
+  `,b=await h(a),c=b?.calendarCollection?.items?.[0];return c?{sys:{id:c.sys?.id},fields:{calendarPdf:j(c.calendarPdf)}}:null}async function v(){let a=`
+    query GetAllDownloadableFiles($limit: Int!, $skip: Int!) {
+      downloadableFileCollection(limit: $limit, skip: $skip, order: sys_publishedAt_DESC) {
+        items {
+          sys {
+            id
+          }
+          fileDescription
+          downloadableFile {
+            url
+            title
+            description
+            width
+            height
+          }
+        }
+      }
+    }
+  `;return(await i({query:a,root:"downloadableFileCollection",limit:50})).map(n)}}};
